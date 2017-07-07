51baf40d61767234c4de6050c2078131ee9d21fc
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-07-07 12:21:40 +0300

    user, styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,20 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-4">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
                 <div class="panel-body">
-                    You are logged in!
+                  <div class="profile-picture">
+                    <img class="img-circle img-responsive" src="https://www.gravatar.com/avatar/{{ md5($user->email) }}?d=retro&s=200" alt="">
+                  </div>
+                  <div class="profile-info">
+                    <div class="profile-username">
+                      {{ $user->name }}
+                    </div>
+                    <div class="profile-score">
+                      {{ $user->score }}
+                    </div>
+                  </div>
                 </div>
             </div>
         </div>
